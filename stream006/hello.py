96adae845a272c5568a0bd0ada4a02f7ecb1c386
Brian Seymour <imbrianseymour@gmail.com>
2018-10-27 11:53:53 -0500

    code

--- stream006/hello.py
+++ stream006/hello.py
@@ -1,7 +1,19 @@
-name1 = 'brian'
+# do assignments
+name1 = 'engineer man'
 name2 = 'pdog'
 name3 = 'duckie'
 
+# print out stuff
+print(name1)
+print(name2)
+print(name3)
+
+# reassign
+name1 = 'pizza'
+name2 = 'wolf'
+name3 = 'brtwrst'
+
+# print out stuff again
 print(name1)
 print(name2)
 print(name3)
