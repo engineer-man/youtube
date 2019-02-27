b4f5183df9bac3868499a2e574d2c1a64f7deee5
Abhishek Kumar <abhishek.kumar.7679@gmail.com>
2019-02-27 23:37:00 +0530

    all bugs fixed.

--- 015/snake.py
+++ 015/snake.py
@@ -8,15 +8,15 @@ w = curses.newwin(sh, sw, 0, 0)
 w.keypad(1)
 w.timeout(100)
 
-snk_x = sw/4
-snk_y = sh/2
+snk_x = sw//4
+snk_y = sh//2
 snake = [
     [snk_y, snk_x],
     [snk_y, snk_x-1],
     [snk_y, snk_x-2]
 ]
 
-food = [sh/2, sw/2]
+food = [sh//2, sw//2]
 w.addch(food[0], food[1], curses.ACS_PI)
 
 key = curses.KEY_RIGHT
