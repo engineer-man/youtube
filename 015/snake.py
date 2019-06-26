f43a9088dab436c358778a5f9c2087a14d76cc5c
Shreyas Gupta <technology.shreyas@gmail.com>
2019-06-26 23:05:17 +0530

    Typecast food and snake vars to integers for python3

--- 015/snake.py
+++ 015/snake.py
@@ -17,7 +17,7 @@ snake = [
 ]
 
 food = [sh/2, sw/2]
-w.addch(food[0], food[1], curses.ACS_PI)
+w.addch(int(food[0]), int(food[1]), curses.ACS_PI)
 
 key = curses.KEY_RIGHT
 
@@ -53,6 +53,6 @@ while True:
         w.addch(food[0], food[1], curses.ACS_PI)
     else:
         tail = snake.pop()
-        w.addch(tail[0], tail[1], ' ')
+        w.addch(int(tail[0]), int(tail[1]), ' ')
 
-    w.addch(snake[0][0], snake[0][1], curses.ACS_CKBOARD)
+    w.addch(int(snake[0][0]), int(snake[0][1]), curses.ACS_CKBOARD)
