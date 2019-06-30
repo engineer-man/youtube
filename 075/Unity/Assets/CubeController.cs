95f367f679f6210512ae1e5e7906c68ab1f3e352
Brian Seymour <imbrianseymour@gmail.com>
2019-06-30 00:10:33 -0500

    new episode

---
+++ 075/Unity/Assets/CubeController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeController : MonoBehaviour {
+
+    enum Direction { LEFT, RIGHT };
+
+    public GameObject cube;
+
+    Direction dir = Direction.RIGHT;
+
+    void Update() {
+        cube.transform.Rotate(.2f, -.3f, .1f);
+
+        float z = Mathf.Clamp(cube.transform.position.z, -15f, 15f);
+
+        if (z == -15f)
+            dir = Direction.RIGHT;
+        if (z == 15f)
+            dir = Direction.LEFT;
+
+        z += dir == Direction.RIGHT ? .08f : -.08f;
+
+        Vector3 position = new Vector3(
+            cube.transform.position.x,
+            cube.transform.position.y,
+            z
+        );
+
+        cube.transform.position = position;
+    }
+
+}
---
+++ 075/Unity/Assets/InfiniteRotate.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InfiniteRotate : MonoBehaviour {
+
+    // Update is called once per frame
+    void Update() {
+        transform.Rotate(.2f, -.3f, .1f);
+    }
+
+}
