8c004d173a04838d9b86100756f49ee0f760dda8
Brian Seymour <imbrianseymour@gmail.com>
2020-02-16 15:59:02 -0600

    new ep

---
+++ 096/commands.sh
@@ -0,0 +1,46 @@
+#!/usr/bin/env bash
+
+# tree navigation
+pwd
+cd
+cd /
+cd ..
+ls
+ls -l
+
+# file/directory manipulation
+touch
+mv
+cp
+cp -r
+rm
+rm -r
+mkdir
+rmdir
+
+# reading/editing files
+cat
+nano
+vim
+less
+more
+tail -f
+
+# super user
+sudo
+sudo -s
+
+# miscellaneous
+clear
+reset
+exit
+poweroff
+reboot
+man
+history
+history -c
+
+# tips and tricks
+- hit up for last command
+- hit tab for autocomplete
+- use man for documentation
