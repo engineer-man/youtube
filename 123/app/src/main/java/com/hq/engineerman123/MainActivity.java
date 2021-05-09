3cfc3743af02ba08d738bdaef46df388107630bf
Brian Seymour <imbrianseymour@gmail.com>
2021-05-09 12:55:51 -0500

    latest videos

--- 123/app/src/main/java/com/hq/engineerman123/MainActivity.java
+++ 123/app/src/main/java/com/hq/engineerman123/MainActivity.java
@@ -2,7 +2,6 @@ package com.hq.engineerman123;
 
 import android.annotation.SuppressLint;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
