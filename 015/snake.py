d21a608a1c391954a66274f8e9ef90e05de4165f
ESHUN <59277986+eSHUN1408@users.noreply.github.com>
2022-03-19 21:46:49 +0000

    Update snake.py

--- 015/snake.py
+++ 015/snake.py
@@ -13,8 +13,7 @@ snk_y = sh/2
 snake = [
     [snk_y, snk_x],
     [snk_y, snk_x-1],
-    [snk_y, snk_x-2]
-]
+    [snk_y, snk_x-2]]
 
 food = [sh/2, sw/2]
 w.addch(int(food[0]), int(food[1]), curses.ACS_PI)
